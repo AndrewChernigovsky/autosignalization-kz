f93b68ed7c6f007e88ac94504d151e3cc0b74af0
Andrew Izosimov <chernigovsky108@gmail.com>
2024-10-18 01:18:04 +0300

    stores

--- stores/navigation.ts
+++ stores/navigation.ts
@@ -1,23 +1,25 @@
 // stores/products.ts
-import { defineStore } from 'pinia';
-import { ref } from 'vue';
-import type {NavigationLinksType} from '@/types/NavigationLinksType';
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import type { NavigationLinksType } from '@/types/NavigationLinksType'
 
 export const useNavigationStore = defineStore('product', () => {
   const navigationLinks = ref<NavigationLinksType[]>([
-    {name: 'Главная', path: '/'},
-    {name: 'О нас', path: '/about'},
-  ]);
+    { name: 'Главная', path: '/' },
+    { name: 'О нас', path: '/about' },
+  ])
 
   function setNavigationLink(newLink: NavigationLinksType) {
-    navigationLinks.value.push(newLink);
+    navigationLinks.value.push(newLink)
   }
 
   function getNavigationLinks() {
-    return navigationLinks.value;
+    return navigationLinks.value
   }
 
   return {
-    navigationLinks,  setNavigationLink, getNavigationLinks
+    navigationLinks,
+    setNavigationLink,
+    getNavigationLinks,
   }
-});
+})
