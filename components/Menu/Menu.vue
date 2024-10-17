9ee61957a2ab620ec9416b6cf094add751acf9de
Andrew Izosimov <chernigovsky108@gmail.com>
2024-10-18 01:05:48 +0300

    edit Readme.md

---
+++ components/Menu/Menu.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="nav">
+    <ul>
+      <li v-for="link in navLinks" :key="link.path">
+        <nuxt-link :to="link.path">{{ link.name }}</nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { useNavigationStore } from '@/stores/navigation'
+import type { NavigationLinksType } from '~/types/NavigationLinksType'
+
+const navLinks = ref<NavigationLinksType[]>([])
+const navigationStore = useNavigationStore()
+
+const getNavLinks = () => {
+  return navigationStore.getNavigationLinks()
+}
+onMounted(async () => {
+  console.log(getNavLinks())
+  navLinks.value = getNavLinks()
+})
+</script>
+<style lang="scss"></style>
