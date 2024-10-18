fea722313094d3ad12c59ee5af6d80ae2a86e218
MamkinCoder <aleksey.aleksandrovich.mc@gmail.com>
2024-10-18 11:21:59 +0300

    Create ImagesType.ts add sertificates store

---
+++ stores/sertificates.ts
@@ -0,0 +1,26 @@
+// stores/products.ts
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import type { ImagesType } from '@/types/ImagesType'
+
+export const useSertificatesStore = defineStore('sertificates', () => {
+  const sertificationImages = ref<ImagesType[]>([
+    {url: "#", description: "Сертификат 1"},
+    {url: "#", description: "Сертификат 2"},
+    {url: "#", description: "Сертификат 3"},
+  ])
+
+  function setSertificates(newImage: ImagesType) {
+    sertificationImages.value.push(newImage)
+  }
+
+  function getSertificatesImages() {
+    return sertificationImages.value
+  }
+
+  return {
+    sertificationImages,
+    setSertificates,
+    getSertificatesImages,
+  }
+})
