fdb82d2960ae75dd5c1bf268d539784905536556
Izosimov_Andrew <chernigovsky108@gmail.com>
2024-10-22 19:37:37 +0300

    dropdown

---
+++ components/UI/Dropdown/Dropdown.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="dropdown">
+    <button type="button" @click="toggleList">
+      <span class="visually-hidden">{{ text }}</span>
+    </button>
+    <ul class="list" :class="{ 'is-open': isOpened }" ref="dropdownList">
+      <li v-for="item in items" :key="item">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref, onMounted, nextTick } from 'vue'
+
+const isOpened = ref<boolean>(false)
+const items = ref<number[]>([1, 1, 2, 2])
+
+const text = computed(() => {
+  return isOpened.value ? 'Закрыть список' : 'Раскрыть список'
+})
+
+function toggleList() {
+  isOpened.value = !isOpened.value
+}
+
+const dropdownList = ref<HTMLElement | null>(null)
+
+onMounted(() => {
+  nextTick(() => {
+    if (dropdownList.value) {
+      dropdownList.value.style.maxHeight = isOpened.value
+        ? `${dropdownList.value.scrollHeight}px`
+        : '0px'
+      dropdownList.value.style.transition = 'max-height 0.3s ease-in-out'
+    }
+  })
+})
+
+watch(isOpened, (newValue) => {
+  if (dropdownList.value) {
+    dropdownList.value.style.maxHeight = newValue
+      ? `${dropdownList.value.scrollHeight}px`
+      : '0px'
+  }
+})
+</script>
+
+<style lang="scss">
+.dropdown {
+  position: relative;
+}
+
+.list {
+  overflow: hidden;
+  max-height: 0;
+  transition: max-height 0.3s ease-in-out;
+}
+
+.is-open {
+  max-height: none;
+}
+</style>
