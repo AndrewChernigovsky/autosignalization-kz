6ac92d34db6cab3f4019d838c511d0f7844fbaad
Izosimov_Andrew <chernigovsky108@gmail.com>
2024-10-22 19:50:43 +0300

    intro

---
+++ components/sections/Intro/Intro.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="intro">
+    <div class="container">
+      <h1>
+        АВТОСИГНАЛИЗАЦИИ <br />
+        АВТОЗАПУСК GSM GPS
+      </h1>
+      <br />
+      <ul>
+        <li>ПРОДАЖА</li>
+        <li>УСТАНОВКА</li>
+        <li>РЕМОНТ</li>
+      </ul>
+    </div>
+  </section>
+</template>
+<script setup lang="ts"></script>
+<style lang="scss"></style>
