8a3d47deb66485477e934aab805c54fc87d62ad7
Izosimov_Andrew <chernigovsky108@gmail.com>
2024-10-24 20:24:38 +0300

    range component

---
+++ components/UI/Range/Range.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <input type="range" id="myRange" min="100" max="900000" v-model="value" />
+    <span>100</span>
+    <span>{{ value }}</span>
+    <span>90000</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const value = ref<number>(100)
+
+console.log(value.value)
+</script>
+<style lang="scss"></style>
