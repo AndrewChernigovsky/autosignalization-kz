b0fb05744062d6f56f8f644157e221c1e4eff441
MamkinCoder <aleksey.aleksandrovich.mc@gmail.com>
2024-10-26 18:57:50 +0300

    fix confilct

--- pages/index.vue
+++ pages/index.vue
@@ -17,15 +17,10 @@
     <YButton :ytype="ButtonsEnum.secondary">Подробнее</YButton>
     <YButton :ytype="ButtonsEnum.primary">Подробнее</YButton>
     <YButton :ytype="ButtonsEnum.tabButton">Подробнее</YButton>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 84c96eb7081da126f1a79bdc43c30b62ddc99e8b
     <CountButton
       :quantity="5"
       @get-quantity="(quantity: number) => console.log(quantity)"
     />
-<<<<<<< HEAD
     <Dropdown :title="'Магазин'" />
     <Checkbox
       :item="{
@@ -45,40 +40,53 @@
         text: 'управление с телефона',
       }"
     />
-=======
     <Dropdown
       :title="'Магазин sdfsdfsdfsd dfsssssssssssss sssssssssssssss sssssssssssssssssssss sssssssssssssssssssssssssss'"
     />
-=======
-    <CountButton :quantity="5" @get-quantity="(quantity: number) => console.log(quantity)" />
+    <CountButton
+      :quantity="5"
+      @get-quantity="(quantity: number) => console.log(quantity)"
+    />
     <Dropdown :title="'Магазин'" />
-<<<<<<< HEAD
     <Checkbox
-      :item="{ type: 'checkbox', name: 'checkbox', id: 'checkbox', disabled: false, text: 'управление с телефона' }" />
+      :item="{
+        type: 'checkbox',
+        name: 'checkbox',
+        id: 'checkbox',
+        disabled: false,
+        text: 'управление с телефона',
+      }"
+    />
     <Checkbox
-      :item="{ type: 'checkbox', name: 'checkbox1', id: 'checkbox1', disabled: true, text: 'управление с телефона' }" />
->>>>>>> 7c513de884e56e1fc58cd115ec700c37986b5226
-=======
-    <Checkbox :item="{
-      type: 'checkbox',
-      name: 'checkbox',
-      id: 'checkbox',
-      disabled: false,
-      text: 'управление с телефона',
-    }" />
-    <Checkbox :item="{
-      type: 'checkbox',
-      name: 'checkbox1',
-      id: 'checkbox1',
-      disabled: true,
-      text: 'управление с телефона',
-    }" />
->>>>>>> 84c96eb7081da126f1a79bdc43c30b62ddc99e8b
-
+      :item="{
+        type: 'checkbox',
+        name: 'checkbox1',
+        id: 'checkbox1',
+        disabled: true,
+        text: 'управление с телефона',
+      }"
+    />
+    <Checkbox
+      :item="{
+        type: 'checkbox',
+        name: 'checkbox',
+        id: 'checkbox',
+        disabled: false,
+        text: 'управление с телефона',
+      }"
+    />
+    <Checkbox
+      :item="{
+        type: 'checkbox',
+        name: 'checkbox1',
+        id: 'checkbox1',
+        disabled: true,
+        text: 'управление с телефона',
+      }"
+    />
     <Range />
     <Input :ytype="InputEnums.email" placeholder="Введите ваш адрес" />
     <Select @click="select" />
->>>>>>> 44b304cfd9a7d878b6add3ab71917507a3eb0361
   </ClientOnly>
 </template>
 <script lang="ts" setup>
@@ -86,19 +94,13 @@ import { onMounted } from 'vue'
 import { useUserStore } from '@/stores/user'
 import { ButtonsEnum } from '~/enums/ButtonsEnum'
 import Intro from '~/components/sections/Intro/Intro.vue'
-<<<<<<< HEAD
 import Breadcrumbs from '@/components/Breadcrumbs.vue'
-=======
 import Advantages from '~/components/sections/Advantages/Advantages.vue'
 import Services from '~/components/sections/Services/Services.vue'
 import Sertificates from '~/components/sections/Sertificates/Sertificates.vue'
 import PresentVideo from '~/components/sections/PresentVideo/PresentVideo.vue'
 import { InputEnums } from '~/enums/InputEnums'
-<<<<<<< HEAD
-import CarsBrand from '~/components/CarsBrand/CarsBrandItem.vue'
-=======
->>>>>>> 44b304cfd9a7d878b6add3ab71917507a3eb0361
->>>>>>> 84c96eb7081da126f1a79bdc43c30b62ddc99e8b
+// import CarsBrand from '~/components/CarsBrand/CarsBrandItem.vue'
 
 const userStore = useUserStore()
 const select = (evt: any) => console.log(evt.target.textContent)
