2b9df9a8e7b266573dd71260df03a1454054788f
MamkinCoder <aleksey.aleksandrovich.mc@gmail.com>
2024-10-27 15:41:22 +0300

    Complite mobile form styles

--- components/sections/FeedbackForm/FeedbackForm.vue
+++ components/sections/FeedbackForm/FeedbackForm.vue
@@ -5,21 +5,16 @@
       <form class="form" action="#" method="POST">
         <div class="brands">
           <h3 class="title m-0">Марка:</h3>
-          <ul class="list list-style-none">
-            <li class="item" v-for="image in brandsImages">
+          <ul class="brand-list list-style-none">
+            <li class="brand-item" v-for="image in brandsImages">
               <input
-                class="input m-0"
+                class="brand-input m-0"
                 type="radio"
                 name="brand"
                 :id="image.id"
               />
-              <label class="label" :for="image.id">
-                <CarsBrandItem
-                  class="image"
-                  :image="image"
-                  :width="90"
-                  :height="90"
-                />
+              <label class="brand-label" :for="image.id">
+                <CarsBrandItem class="brand-image" :image="image" />
                 <span class="visually-hidden">{{ image.id }}</span>
               </label>
             </li>
@@ -28,11 +23,29 @@
         <div class="inputs-container">
           <ul class="list list-style-none">
             <li class="inupt-item" v-for="input in inputs">
-              <label class="title">{{ input.title }}</label>
-              <input class="input-form" type="text" :id="input.id" />
+              <label class="item-label" :for="input.id">{{
+                input.title
+              }}</label>
+              <div class="input-wrapper">
+                <input
+                  class="input-form"
+                  type="text"
+                  :id="input.id"
+                  :placeholder="input.placeholder"
+                />
+              </div>
             </li>
           </ul>
         </div>
+        <div class="text-area-container">
+          <label class="item-label" for="form-text-aria">
+            Ваша сообщение:
+          </label>
+          <textarea class="form-text-aria" id="form-text-aria">
+Вы купили автомобиль и желаете защитить его, установив сигнализацию?  Вы любите комфорт и хотите установить автозапуск на Ваше авто?  Вам необходимо отслеживать Ваш транспорт по GPS?  Обращайтесь к нам, и мы поможем Вам решить эти задачи!
+          </textarea>
+        </div>
+        <button class="form-btn" type="submit">Отправить заявку</button>
       </form>
     </div>
   </section>
@@ -47,87 +60,153 @@ const inputs = [
   {
     id: 'Модель',
     title: 'Модель автомобиля:',
+    placeholder: 'Vesta',
   },
   {
     id: 'Год',
     title: 'Год выпуска:',
+    placeholder: '2024',
   },
   {
     id: 'Имя',
     title: 'Имя:',
+    placeholder: 'Ivanov Ivan Ivanovich',
   },
   {
     id: 'Телефон',
     title: 'Телефон:',
+    placeholder: '+7 (777) 77 77 777',
   },
 ]
 </script>
 <style scoped lang="scss">
 .section {
-  background-color: #000;
-  padding: 60px 0;
+  padding: 30px 0;
+  background-color: #000000;
+  color: #ffffff;
 }
 
 .section-title {
-  margin-bottom: 30px;
+  font-family: 'Oswald', 'Arial', sans-serif;
+  text-align: center;
+  font-size: 24px;
+  line-height: 34px;
+  margin-bottom: 20px;
 }
 
 .form {
-  display: grid;
-  grid-template-columns: repeat(12, 102px);
-  column-gap: 16px;
-  row-gap: 20px;
+  display: flex;
+  flex-direction: column;
+  row-gap: 10px;
 }
 
 .title {
-  font-family: 'DINPRO', 'Arial', sans-serif;
-  font-size: 48px;
-  line-height: 60px;
+  font-size: 24px;
+  line-height: 30px;
   margin-bottom: 10px;
 }
 
-.brands {
-  grid-column: 1 / -1;
+.brand-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+}
+
+.brand-item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-width: 40px;
+  min-height: 40px;
+  padding: 5px;
+  max-width: 50px;
+  max-height: 50px;
+  background-color: #ffffff;
+  border-radius: 5px;
+  cursor: pointer;
+  position: relative;
+}
+
+.brand-input {
+  appearance: none;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+
+  & + .brand-label {
+    opacity: 0.7;
+    transition: opacity 0.3s ease;
+  }
+
+  &:checked + .brand-label {
+    opacity: 1;
+    transition: opacity 0.3s ease;
+  }
+}
+
+.brand-image {
+  width: 40px;
+  height: 40px;
 }
 
 .list {
   display: flex;
-  flex-wrap: wrap;
-  gap: 28px;
+  flex-direction: column;
+  gap: 20px;
 }
 
-.item {
-  position: relative;
-}
-
-.label {
+.inupt-item {
   display: flex;
-  min-width: 150px;
-  min-height: 150px;
-  justify-content: center;
-  align-items: center;
-  opacity: 0.7;
-  transition: all 0.3s ease;
-  background-color: #fff;
+  flex-direction: column;
+}
+
+.item-label {
+  font-size: 24px;
+  line-height: 30px;
+  margin-bottom: 10px;
+}
+
+.input-wrapper {
+  padding: 10px;
+  width: 100%;
+  background-color: #141212;
   border-radius: 20px;
 }
 
-.input {
-  appearance: none;
-  position: absolute;
+.input-form {
+  font-size: 24px;
+  line-height: 30px;
+  width: 100%;
+  padding: 10px;
+  border: 0;
+  background-color: #363535;
+  border-radius: 20px;
+  color: #ffffff;
+}
 
-  &:hover + .label {
-    transition: all 0.3s ease;
-    border-radius: 20px;
-    opacity: 1;
-    box-shadow: inset 0 0 0 6px #000;
-  }
+.form-btn {
+  min-height: 48px;
+  font-size: 20;
+  line-height: 26px;
+  font-weight: 700;
+  background-image: linear-gradient(180deg, #280000 0%, #ff0000 100%);
+  border: 0;
+  border: 1px solid #c9c9c9;
+  border-radius: 5px;
+  padding: 0;
+  text-align: center;
+  color: #fff;
+}
 
-  &:checked + .label {
-    transition: all 0.3s ease;
-    border-radius: 20px;
-    opacity: 1;
-    box-shadow: inset 0 0 0 6px #000;
-  }
+.form-text-aria {
+  font-size: 16px;
+  line-height: 20px;
+  width: 100%;
+  min-height: 340px;
+  padding: 10px;
+  margin-top: 10px;
+  border-radius: 5px;
+  color: #fff;
+  background-color: #424242;
 }
 </style>
