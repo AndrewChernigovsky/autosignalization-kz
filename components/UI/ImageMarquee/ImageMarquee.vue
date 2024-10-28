fe20107d3ac15e4da435e4d7c9ee04c6b029c8f9
Izosimov_Andrew <chernigovsky108@gmail.com>
2024-10-28 16:56:29 +0300

    fix carsbrand

---
+++ components/UI/ImageMarquee/ImageMarquee.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="marquee">
+    <ul class="marquee-content list">
+      <li
+        class="image-container item"
+        v-for="(image, index) in images"
+        :key="index"
+      >
+        <img
+          class="image"
+          :src="image.image"
+          :alt="image.title"
+          width="140"
+          height="140"
+        />
+      </li>
+      <li
+        class="image-container item"
+        v-for="(image, index) in images"
+        :key="'duplicate-' + index"
+      >
+        <img
+          class="image"
+          :src="image.image"
+          :alt="image.title"
+          width="140"
+          height="140"
+        />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  images: {
+    type: Array as () => {
+      id: string
+      image: string
+      imageWeb: string
+      title: string
+    }[],
+    required: true,
+  },
+})
+</script>
+
+<style scoped>
+.list {
+  display: flex;
+  gap: 30px;
+  overflow: hidden;
+}
+
+.item {
+  background-color: white;
+  padding: 30px;
+  border-radius: 30px;
+  min-width: 200px;
+  min-height: 200px;
+  max-width: 300px;
+  max-height: 300px;
+
+  @media screen and (min-width: 768px) {
+    min-width: 300px;
+    min-height: 300px;
+    & .image {
+      width: 240px;
+      height: 240px;
+    }
+  }
+}
+.marquee {
+  overflow: hidden;
+  white-space: nowrap;
+  box-sizing: border-box;
+}
+
+.marquee-content {
+  display: inline-block;
+  animation: marquee 120s linear infinite;
+}
+
+.image-container {
+  display: inline-block;
+  margin-right: 20px;
+}
+
+@keyframes marquee {
+  from {
+    transform: translateX(100%);
+  }
+  to {
+    transform: translateX(-100%);
+  }
+}
+</style>
