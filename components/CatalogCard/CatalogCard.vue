37f59f48817424df1fc6f3c7e847d53342887059
MamkinCoder <aleksey.aleksandrovich.mc@gmail.com>
2024-11-08 15:47:31 +0300

    start catalog page

---
+++ components/CatalogCard/CatalogCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card-item">
+    <div class="content">
+      <div class="picture-container">
+        <NuxtPicture />
+      </div>
+      <h1 class="title m-0">{{}}</h1>
+      <p class="price">{{}}</p>
+      <div class="buttons-container">
+        <YButton :link="true" :btn="false" :path="'/catalog'">{{}}</YButton>
+        
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  product: {
+    type: Object,
+    default: () => '',
+  },
+})
+</script>
+<style lang="scss"></style>
--- pages/catalog.vue
+++ pages/catalog.vue
@@ -1,5 +1,45 @@
 <template>
-  <div>catalog</div>
+  <div class="page-container">
+    <div class="container wrapper">
+      <section class="catalog">
+        <h1 class="title m-0">АВТОСИГНАЛИЗАЦИИ С АВТОЗАПУСКОМ</h1>
+        <div class="catalog-wrapper">
+          <ul class="list">
+            <li class="item"></li>
+          </ul>
+          <div class="filter-form">
+            <form action="#" class="form"></form>
+          </div>
+          <div class="sort">
+            <h2 class="m-0">Сортировка</h2>
+            <select name="select" id="catalog-select"></select>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
 </template>
-<script setup></script>
-<style lang=""></style>
+<script setup lang="ts">
+import { useProductStore } from '@/stores/products'
+import { ref, onMounted } from 'vue'
+import type { ProductType } from '@/types/ProductType'
+
+const productStore = useProductStore()
+
+const products = ref<ProductType>()
+
+onMounted(() => {
+  products.value = productStore.getProducts()
+})
+</script>
+<style lang="scss">
+.page-container {
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(#010308, #000208);
+}
+
+.catalog {
+  color: #fff;
+}
+</style>
